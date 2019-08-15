137a5bf2aaf7cfa74cdd9c3cdcced759d74e43af
yonnie0106 <53652417+yonnie0106@users.noreply.github.com>
2019-08-15 11:46:09 -0500

    Update HW2
    Completed hw2
    Added comments

--- homeworks/hw2/hw2_yeon.py
+++ homeworks/hw2/hw2_yeon.py
@@ -6,46 +6,45 @@ import os
 import csv
 
 
-
 with open('hw2_yeon.csv', 'w', encoding='utf-8') as f:
 	w = csv.DictWriter(f, fieldnames = ("Title", "Date", "Issues", "Number_of_signitures"))
 	w.writeheader()
-	url = 'https://petitions.whitehouse.gov/petitions'
+	url = 'https://petitions.whitehouse.gov/petitions' 
 	html = urllib.request.urlopen(url)
 	soup = BeautifulSoup(html.read())
 	petition={}
 	tags = soup.select('h3 > a')
 	for tag in tags:
 		try:
-			petition_page = urllib.request.urlopen('https://petitions.whitehouse.gov' + tag.get('href'))
-		except:
+			petition_page = urllib.request.urlopen('https://petitions.whitehouse.gov' + tag.get('href')) # individual links to each petition
+		except:	
 			print(Error)
 			continue
-		petition_html = BeautifulSoup(petition_page.read())
-		petition["Title"] = petition_html.find('h1').get_text()
-		petition["Date"] = petition_html.find('h4').get_text()
-		petition["Issues"] = ', '.join(map(str, [i.text for i in petition_html.find('div', {'class' : 'content'}).find_all('h6')]))
-		petition["Number_of_signitures"] = petition_html.find('span', {'class' : 'signatures-number'}).get_text()
-		w.writerow(petition)
+		petition_html = BeautifulSoup(petition_page.read())	
+		petition["Title"] = petition_html.find('h1').get_text()	# Title 
+		petition["Date"] = petition_html.find('h4').get_text()  # Date
+		petition["Issues"] = ', '.join(map(str, [i.text for i in petition_html.find('div', {'class' : 'content'}).find_all('h6')])) # Issues
+		petition["Number_of_signitures"] = petition_html.find('span', {'class' : 'signatures-number'}).get_text() # Number of Signitures
+		w.writerow(petition) # write to .csv file
 
-	for i in range(1, 5):
+	for i in range(1, 5): # for extracting information of additional petitions over pages 1 to 4  
 		try:
-			more_html = urllib.request.urlopen('https://petitions.whitehouse.gov/?page=' + str(i))
-			more_soup = BeautifulSoup(more_html.read())
+			more_html = urllib.request.urlopen('https://petitions.whitehouse.gov/?page=' + str(i)) # additional pages of the list of petitions 
+			more_soup = BeautifulSoup(more_html.read()) 
 		except:
 			print(Error)
 		more_tags = more_soup.select('h3 > a')
 		for tag in more_tags: 
 			try: 
-				more_petition_page = urllib.request.urlopen('https://petitions.whitehouse.gov' + tag.get('href'))
+				more_petition_page = urllib.request.urlopen('https://petitions.whitehouse.gov' + tag.get('href')) # links to each additional petition
 				print
 			except:
 				print(Error)
 				continue
 			more_petition_html = BeautifulSoup(more_petition_page.read())
-			petition["Title"] = more_petition_html.find('h1').get_text()
-			petition["Date"] = more_petition_html.find('h4').get_text()
-			petition["Issues"] = ', '.join(map(str, [i.text for i in more_petition_html.find('div', {'class' : 'content'}).find_all('h6')]))
-			petition["Number_of_signitures"] = more_petition_html.find('span', {'class' : 'signatures-number'}).get_text()
-			w.writerow(petition)
+			petition["Title"] = more_petition_html.find('h1').get_text() # Title
+			petition["Date"] = more_petition_html.find('h4').get_text()	 # Date
+			petition["Issues"] = ', '.join(map(str, [i.text for i in more_petition_html.find('div', {'class' : 'content'}).find_all('h6')])) # Issues
+			petition["Number_of_signitures"] = more_petition_html.find('span', {'class' : 'signatures-number'}).get_text() # Number of Signitures
+			w.writerow(petition) # write to .csv file
 
