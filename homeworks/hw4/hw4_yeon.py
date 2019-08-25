d8d3d5771306421f62c1dc8f5523f8bbc53e39aa
yonnie0106 <53652417+yonnie0106@users.noreply.github.com>
2019-08-25 02:29:24 -0500

    Update HW4
    Updated hw4_yeon.py

--- homeworks/hw4/hw4_yeon.py
+++ homeworks/hw4/hw4_yeon.py
@@ -8,12 +8,12 @@ def InsertionSort(numbers):
 				numbers.pop(i)
 				numbers.insert(j, value)
 				break # break the loop after the insertion
-		print(numbers)
+		#print(numbers)
 	return numbers
 
 ## Testing InsertionSort
 test = [3, 7, 4, 9, 5, 2, 6, 8, 1]
-InsertionSort(test)
+#InsertionSort(test)
 
 
 ### Merge Sort : O(nlogn)
@@ -22,12 +22,12 @@ InsertionSort(test)
 def MergeSort(numbers):
 	if len(numbers) == 1: 
 		return numbers  # Stop the recursion(spliting the list) when sublists have only one element. 
-	print("Dividing", numbers)
+	#print("Dividing", numbers)
 	mid = len(numbers)//2
 	g1 = MergeSort(numbers[:mid])
 	g2 = MergeSort(numbers[mid:])
 	sorted_numbers = []
-	print("Merging", g1, "and", g2)
+	#print("Merging", g1, "and", g2)
 	while len(g1) > 0 and len(g2) > 0:
 		if g1[0] < g2[0]:
 			sorted_numbers.append(g1[0])
@@ -43,10 +43,48 @@ def MergeSort(numbers):
 
 ## Testing MergeSort
 test = [3, 7, 4, 9, 5, 2, 6, 8, 1]
-print(MergeSort(test))
+#print(MergeSort(test))
 
 
 
+##### Comparing Algorithms #####
+
+### Define a function that measures runtime 
+import random 
+from timeit import default_timer as timer
+
+def Sorting_timer(function, n):
+	result = []
+	while n <= 2000: 
+		l = random.sample(range(0, 3000), n)
+		start = timer()
+		function(l)
+		stop = timer()
+		result += [stop-start]
+		n += 100
+	return result
+
+M_result = Sorting_timer(MergeSort, 100)
+I_result = Sorting_timer(InsertionSort, 100)
 
 
+### Create a graph
 
+
+import matplotlib.pyplot as plt
+
+x = [i for i in range(100, 2001, 100)] ## # of elements in list
+y1 = M_result ## time
+y2 = I_result
+plt.subplots_adjust(left = .13, right = .95, top = .9, bottom = .3)
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.legend(['Merge', 'Insertion'], loc = "upper left", prop = {"size":10})
+plt.ylabel("Runtime(seconds)")
+plt.xlabel("the Number of elements in the list")
+plt.title("The Effect of Different Sort Algorithms on Runtime")
+txt = """
+Maybe a description here
+"""
+plt.figtext(.5, .05, txt, fontsize = 10, ha = "center")
+plt.savefig('hw4_yeon.pdf')
